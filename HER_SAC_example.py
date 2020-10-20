543fae2b4e1ba46a055b34bc64741c8ef933c294
kevslinger <tug74186@temple.edu>
2020-10-20 17:24:42 -0400

    only td3 this time

--- HER_SAC_example.py
+++ HER_SAC_example.py
@@ -30,7 +30,7 @@ class TensorboardCallback(BaseCallback):
 
 
 
-model_list = [SAC, TD3, DDPG]#, DQN]
+model_list = [TD3]#[SAC, TD3, DDPG]#, DQN]
 for model in model_list:     
     model_class = model
     model_str = str(model_class)[-6:-2].strip('.')
