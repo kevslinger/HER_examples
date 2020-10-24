a00cdbacfcd47136e27946602ecb287e92faf03a
kevslinger <siouxman@me.com>
2020-10-24 16:41:26 -0400

    updated code for betting log dirs

--- HER_SAC_example.py
+++ HER_SAC_example.py
@@ -3,12 +3,13 @@
 import tensorflow as tf
 import numpy as np
 
-from stable_baselines import HER, SAC, DQN, TD3, DDPG
+from stable_baselines import HER, SAC, DQN, TD3 #DDPG
 from stable_baselines.her import GoalSelectionStrategy, HERGoalEnvWrapper
 from stable_baselines.common.bit_flipping_env import BitFlippingEnv
 from stable_baselines.common.callbacks import BaseCallback
 from gym.envs import robotics
-
+import gym
+import highway_env
 
 class TensorboardCallback(BaseCallback):
     def __init__(self, verbose=0):
@@ -29,34 +30,43 @@ class TensorboardCallback(BaseCallback):
         return True
 
 
+def main():
+    model_list = [SAC, TD3]#, DDPG]#, DQN]
+    for model in model_list:
+        model_class = model
+        model_str = str(model_class)[-6:-2].strip('.')
+        #env = robotics.FetchReachEnv()
+        env_string = 'parking-v0'
+        env = gym.make(env_string)
 
-model_list = [SAC, TD3, DDPG]#, DQN]
-for model in model_list:     
-    model_class = model
-    model_str = str(model_class)[-6:-2].strip('.')
-    env = BitFlippingEnv(8, continuous=model_class, max_steps=8)
+        goal_selection_strategy = 'future'
+        if model == SAC:
+            model = HER('MlpPolicy', env, model_class, n_sampled_goal=4,
+                        goal_selection_strategy=goal_selection_strategy, buffer_size=1000000,
+                        ent_coef='auto', batch_size=256, gamma=0.95, learning_rate=0.001, learning_starts=1000,
+                        random_exploration=0.0, policy_kwargs=dict(layers=[256, 256, 256]),
+                        tensorboard_log='./logs/' + env_string + '_' + model_str, verbose=1)
+        elif model == TD3:
+            model = HER('MlpPolicy', env, model_class, n_sampled_goal=4,
+                        goal_selection_strategy=goal_selection_strategy, buffer_size=1000000,
+                        batch_size=256, gamma=0.95, learning_rate=0.001, learning_starts=1000,
+                        tensorboard_log='./logs/' + env_string + '_' + model_str, verbose=1)
+        model.learn(20000)#, callback=TensorboardCallback())
+        model_path = './models/' + env_string + '_' + model_str
+        model.save(model_path)
 
-    goal_selection_strategy = 'final'
+    model = HER.load(model_path, env=env)
 
-    model = HER('MlpPolicy', robotics.FetchReachEnv, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy, tensorboard_log='./logs/conglomerate_FetchReach/' + model_str, verbose=1)
+    obs = env.reset()
+    for _ in range(100):
+        action, _ = model.predict(obs)
+        obs, reward, done, _ = env.step(action)
 
-    model.learn(100000)#, callback=TensorboardCallback())
+        print(reward)
 
-    model_path = './models/her_fetchReach_' + model_str
-    model.save(model_path)
-
-model = HER.load(model_path, env=env)
-
-
-obs = env.reset()
-for _ in range(100):
-    action, _ = model.predict(obs)
-    obs, reward, done, _ = env.step(action)
-
-    print(reward)
-
-    if done:
-        obs = env.reset()
+        if done:
+            obs = env.reset()
 
 
-
+if __name__ == '__main__':
+    main()
